6b373feace9b1eb42871861fbc88ff5017bbb7bb
MMiochi <sewerynmarciniak14@gmail.com>
2025-01-28 14:31:05 +0100

    Działa

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -5,9 +5,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.graphics.Bitmap;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,9 +28,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         createNotificationChannel();
+
         Button button = findViewById(R.id.Button);
         button.setOnClickListener(v->{
             sendNotification();
+        });
+
+        Button Buttonlong = findViewById(R.id.ButtonLong);
+        Buttonlong.setOnClickListener(v->{
+            sendNotificationLong();
+        });
+        Button ButtonPicture = findViewById(R.id.PictureButton);
+        ButtonPicture.setOnClickListener(v->{
+            sendNotificationPicture();
         });
     }
     private void createNotificationChannel() {
@@ -69,5 +81,64 @@ public class MainActivity extends AppCompatActivity {
         NotificationManagerCompat notificationManager =
                 NotificationManagerCompat.from(this);
         notificationManager.notify(1,builder.build());
+    }
+
+
+private void sendNotificationLong(){
+
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+        if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS},1);
+            return;
+        }
+
+
+    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+    PendingIntent pendingIntent = PendingIntent.getActivity(this,0, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+    NotificationCompat.Builder builder =
+            new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.dice2)
+                    .setContentTitle("Długie Powiadomienie 3TP-e")
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum vitae ligula nec aliquam. Suspendisse ac quam odio. Nunc feugiat turpis ut justo volutpat, in efficitur ante condimentum. Proin ante est, aliquam ac orci sit amet, scelerisque aliquam arcu. Integer quis purus nisi. Mauris sit amet accumsan nisi. Integer sed imperdiet mi, luctus gravida est. Praesent condimentum at nunc in posuere. Nam tempus nunc vitae quam pellentesque luctus. Fusce ultrices iaculis dapibus. Morbi nec ultricies nisl. Suspendisse est eros, semper nec velit eu, aliquam volutpat nunc. Fusce orci nulla, feugiat et augue vitae"))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+    NotificationManagerCompat notificationManager =
+            NotificationManagerCompat.from(this);
+    notificationManager.notify(1,builder.build());
+        }
+    }
+    private void sendNotificationPicture(){
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice0);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS},1);
+                return;
+            }
+
+
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this,0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(this, CHANNEL_ID)
+                            .setSmallIcon(R.drawable.dice2)
+                            .setContentTitle("Powiadomienie Obraz 3TPE")
+                            .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true);
+            NotificationManagerCompat notificationManager =
+                    NotificationManagerCompat.from(this);
+            notificationManager.notify(1,builder.build());
+        }
     }
 }
